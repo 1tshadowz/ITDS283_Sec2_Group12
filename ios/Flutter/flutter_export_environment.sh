693d91581a7ec7937ca27732909550ca28f0b07d
Naris Pholpak <phainaris@gmail.com>
2025-04-07 18:44:14 +0700

    dart

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\MU\2nd-year ICT\Mobile\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\MU\2nd-year ICT\Mobile\Mobileapp\ITDS283_Sec2_Group12\watertracker"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
